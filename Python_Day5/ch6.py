f5d780e74b61c617af0f9ddd53e5736fb0261c20
Harshallchavan <harshalchavan413@gmail.com>
2026-09-24 21:31:34 +0530

    Update Python practice

---
+++ Python_Day5/ch6.py
@@ -0,0 +1,12 @@
+#Dictionary And Sets
+
+# Dictionary is a collection of key-value pairs
+
+marks = {
+    "harshal":100,
+    "Rohan":96,
+    "neha":89
+}
+# print(marks,type(marks))
+print(marks["harshal"])
+print(marks["Rohan"])
